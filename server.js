6dfa949800f52589b100aeb3fd9ab3738e5be488
DawsonBolen <dawson120@live.com>
2023-06-14 10:22:43 -0700

    second commit. Added some new functions

--- server.js
+++ server.js
@@ -40,33 +40,80 @@ async function handleOptions() {
         displayEmployees();
         handleOptions();
     } else if (results.command == 'Add a Department') {
-        addDepartment();
-        handleOptions();
+        inquirer.prompt([
+            {
+                message: 'enter the department name',
+                name: 'name',
+                type: 'input',
+            }
+        ]).then((departmentData) => {
+            addDepartment(departmentData);
+            handleOptions();
+        });
+
     } else if (results.command == 'Add a Role') {
-        addRole();
-        handleOptions();
+        inquirer.prompt([
+            {
+                message: 'enter the role name',
+                name: 'name',
+                type: 'input',
+            }
+        ]).then((roleData) => {
+            addRole(roleData);
+            handleOptions();
+        });
+
     } else if (results.command == 'Add an Employee') {
-        addEmployee();
-        handleOptions();
+        inquirer.prompt([
+            {
+                message: 'enter employee name',
+                name: 'name',
+                type: 'input',
+            }
+        ]).then((employeeData) => {
+            addEmployee(employeeData);
+            handleOptions();
+        });
+
     } else if (results.command == 'Update Employee Role') {
-        updateEmployee();
-        handleOptions();
+        inquirer.prompt([
+            {
+                message: 'enter employee name and new role',
+                name: 'name',
+                type: 'input',
+            }
+        ]).then((updatedRoleData) => {
+            updateEmployee(updatedRoleData);
+            handleOptions();
+        });
     } else {
         console.log('error');
     }
 
 }
 
-connection.query(
-  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-  function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
 
-  function displayDepartments(results) {
 
-    }
+function displayDepartments(results) {
+    connection.query(
+        'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+        }
+    );
+}
+
+function addDepartment() {
+    connection.query('INSERT INTO employees SET ?', employee, (err, results) => {
+        if (err) {
+            console.error('Error inserting data:', err);
+            return;
+        }
+        console.log('Data inserted successfully!');
+        console.log('Inserted row ID:', results.insertId);
+    });
+
+}
 
 handleOptions();
