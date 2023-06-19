8cf62bb399b5fe46379068752b91d52f9ff72cf3
DawsonBolen <dawson120@live.com>
2023-06-19 14:48:15 -0700

    more bug fixing

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ async function displayEmployees() {
     // mainMenu();
 
     const query = `
-    SELECT employee.id, employee.firstname, employee.lastname, role.salary, department.name AS department
+    SELECT employee.id, employee.firstname, employee.lastname, role.salary, role.title, department.name AS department
     FROM employee
     LEFT JOIN role ON employee.role_id = role.id
     LEFT JOIN department ON role.department_id = department.id
