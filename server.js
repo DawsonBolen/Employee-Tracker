d177468c19303d249df21723b8ba66a9d353199f
DawsonBolen <dawson120@live.com>
2023-06-19 12:59:12 -0700

    a few more bug fixes. But I'm getting most of them resolved. Should be last or second to last push besides for the push that will add my readme file

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ async function displayRoles() {
 
 async function displayEmployees() {
     const query = `
-    SELECT employee.id, employee.name, role.salary, department.name AS department
+    SELECT employee.id, employee.firstname, employee.lastname, role.salary, department.name AS department
     FROM employee
     LEFT JOIN role ON employee.role_id = role.id
     LEFT JOIN department ON role.department_id = department.id
